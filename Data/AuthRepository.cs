2f3a94efc215ebf5e8523d88cc52c75cad70c505
nerijus-bauza <nerijus.bauza@gmail.com>
2023-06-30 23:33:28 +0300

    feat: implement user register functionality in AuthRepository

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,57 @@ namespace dotnet_rpg.Data
 {
   public class AuthRepository : IAuthRepository
   {
+    private readonly DataContext context;
+    public AuthRepository(DataContext context)
+    {
+      this.context = context;
+    }
     public Task<ServiceResponse<string>> Login(string username, string password)
     {
       throw new NotImplementedException();
     }
 
-    public Task<ServiceResponse<int>> Register(User user, string password)
+    public async Task<ServiceResponse<int>> Register(User user, string password)
     {
-      throw new NotImplementedException();
+      var serviceResponse = new ServiceResponse<int>();
+      if (await UserExists(user.Username))
+      {
+        serviceResponse.Success = false;
+        serviceResponse.Message = $"User '{user.Username}' already exists.";
+        return serviceResponse;
+      }
+      
+      CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+      user.PasswordHash = passwordHash;
+      user.PasswordSalt = passwordSalt;
+      
+      context.Users.Add(user);
+      await context.SaveChangesAsync();
+      
+      serviceResponse.Data = user.Id;
+      serviceResponse.Message = $"User ${user.Username} successfully created";
+
+      return serviceResponse;
     }
 
-    public Task<bool> UserExists(string username)
+    public async Task<bool> UserExists(string username)
     {
-      throw new NotImplementedException();
+      if (await context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using(var hmac = new System.Security.Cryptography.HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
     }
   }
 }
